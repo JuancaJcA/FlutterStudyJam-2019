0d095cab2a783482855cf9009773b6ef28db3187
JuancaJcA <avilafloresjc@gmail.com>
2019-04-03 17:58:51 -0700

    Puntaje e imágenes realizado

--- flutter_says/lib/main.dart
+++ flutter_says/lib/main.dart
@@ -1,62 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: FlutterSays(),
+      ),
+    ),
+  );
+}
 
-class MyApp extends StatelessWidget {
+class FlutterSays extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Container(
+      padding: EdgeInsets.only(top: 100.0),
+      child: Column(
+        children: <Widget>[
+          score(),
+          SizedBox(height: 100),
+          images("assets/images/green.png", "assets/images/red.png"),
+          images("assets/images/yellow.png", "assets/images/blue.png")
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class score extends StatelessWidget {
+  int _points = 1;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Text(
+        '$_points',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            color: Colors.white, fontSize: 70.0, fontWeight: FontWeight.w500),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class images extends StatelessWidget {
+  String image1 = "";
+  String image2 = "";
+  images(this.image1, this.image2);
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  Widget imageContainer(String image) {
+    return Container(
+      height: 120.0,
+      width: 120.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(image), fit: BoxFit.fill),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[imageContainer(image1), imageContainer(image2)],
     );
   }
 }
